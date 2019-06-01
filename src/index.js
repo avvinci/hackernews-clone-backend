f06f63b6c202e31f5d108cd80d388b8f2ed52f71
avvinci <vinciabhinav7@gmail.com>
2019-06-01 15:49:03 +0530

    added link schema and query

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 const {GraphQLServer} = require('graphql-yoga')
 
-const typeDefs = `
-type Query{
-    info: String!
-}
-`
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for graphql'
+}]
+
+
 const resolvers = {
     Query: {
-        info: () =>  `This is the API of a hackernews clone`
+        info: () =>  `This is the API of a hackernews clone`,
+        feed: () => links , 
+    },
+
+    Link : {
+        id : (parent) => parent.id ,
+        url : (parent) => parent.url,
+        description: (parent) => parent.description,
     }
 }
 
 
+
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs : './src/schema.grapgql',
     resolvers,
 })
 
