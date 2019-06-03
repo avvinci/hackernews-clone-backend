326af077f9109ad7e70c9d712c2d93a38c8039f1
avvinci <vinciabhinav7@gmail.com>
2019-06-03 14:38:37 +0530

    signup, login done, error in post auth

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,45 +1,57 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
 
-const { APP_SECRET, getUserId} = require('../utils')
-
-async function signup (parent, args, context, info){
-
-    const password = await bcrypt.hash(args.password,10) 
-    const user  = await context.prisma.createUser({...args, password})
-    const token = jwt.sign({userId: user.id},  APP_SECRET) 
-
+async function signup(parent, args, context, info) {
+    // 1
+    const password = await bcrypt.hash(args.password, 10)
+    // 2
+    const user = await context.prisma.createUser({ ...args, password })
+  
+    // 3
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+    // 4
     return {
-        token, 
-        user,
+      token,
+      user,
     }
-}
-
-async function login(parent, args, context,info){
-    
-    const user = await context.prisma.user({email: args.email}) 
-
-    if(!user){
-        throw new Error('user not found')
+  }
+  
+  async function login(parent, args, context, info) {
+    // 1
+    const user = await context.prisma.user({ email: args.email })
+    if (!user) {
+      throw new Error('No such user found')
     }
-
-    const valid = await bcrypt.compare(args.password,user.password)
-
-    if(!valid){
-        throw new Error('invalid password')
+  
+    // 2
+    const valid = await bcrypt.compare(args.password, user.password)
+    if (!valid) {
+      throw new Error('Invalid password')
     }
-
-    const token = jwt.sign({userId:user.id},  APP_SECRET)
-
+  
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+    // 3
     return {
-        token,
-        user,
+      token,
+      user,
     }
-}
+  }
+  
 
+  function post(parent, args, context, info) {
+    const userId = getUserId(context)
+    return context.prisma.createLink({
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } },
+    })
+  }
 
-module.exports = {
+  module.exports = {
     signup,
     login,
     post,
-}
+  }
