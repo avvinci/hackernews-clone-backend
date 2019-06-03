dcddfb7aa7d37598abd57e1fe892d4fe5283f7b9
avvinci <vinciabhinav7@gmail.com>
2019-06-03 09:07:55 +0530

    added utils and updated files for authentication

--- src/index.js
+++ src/index.js
@@ -29,7 +29,12 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs : './src/schema.graphql',
     resolvers,
-    context : {prisma},
+    context : request => {
+        return{
+            ...request,
+            prisma,
+        }
+    },
 })
 
 server.start(() => console.log('Server is running on port 4000'))
