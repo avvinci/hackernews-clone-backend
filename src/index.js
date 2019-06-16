cb0d5b04ba17a232ff4cd0410ccccc0dd7e49935
avvinci <vinciabhinav7@gmail.com>
2019-06-16 20:00:01 +0530

    added votes subscription , plus small fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
-const Vote = require('./resolvers/Vote')
+const Vote = require('./resolvers/Votes')
 const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,5 +1,9 @@
 function newLinkSubscribe(parent, args, context, info){
-    return context.prisma.$subscribe.link({mutation_in : ['CREATESD'] }).node()
+    return context.prisma.$subscribe.link({mutation_in : ['CREATED'] }).node()
+}
+
+function newVoteSubscribe(parent, args, context , info ){
+    return context.prisma.$subscribe.vote({mutation_in: ['CREATED']}).node()
 }
 
 const newLink = {
@@ -9,7 +13,14 @@ const newLink = {
     },
 }
 
+const newVote = {
+    subscribe : newVoteSubscribe,
+    resolver : payload => {
+        return payload
+    },
+}
+
 module.exports = {
     newLink,
-    
+    newVote,
 }
